9632aa0bad817d0bc29212d913e16d71f7c7cc8a
mutanganajoseph <josephmutangana65@gmail.com>
2024-11-25 15:23:24 +0200

    Updated formSubmit files

--- Js_basics/formSubmit.js
+++ Js_basics/formSubmit.js
@@ -1,49 +1,102 @@
 
 const errorDiv = document.getElementById("error");
+const firstNameError = document.getElementById("firstNameError");
+const seconNameError = document.getElementById("secondNameError");
+const emailError = document.getElementById("emailError");
 const my_field = document.getElementById("myField");
-const submitted = document.getElementById("submitted");
-let back = document.querySelector("a");
-
-function clear_error(){
-    errorDiv.textContent ='';
-}
-
-document.getElementById("firstName").addEventListener("input", clear_error);
-document.getElementById("secondName").addEventListener("input", clear_error);
-document.getElementById("email").addEventListener("input", clear_error);
+const firstHover = document.getElementById("firstHover");
+const secondHover = document.getElementById("secondHover");
+const emailHover = document.getElementById("emailHover");
 
 
-document.getElementById('submit').onclick = function(e){
-    e.preventDefault();
 
-
+function clearError(){
     
-    const first_name = document.getElementById("firstName").value.trim();
-    const second_name = document.getElementById("secondName").value.trim();
-    const emailInput = document.getElementById("email").value.trim();
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[a-zA-Z]{2,}$/
-;
-
    
     
-    if(first_name && second_name && emailInput){
-        my_field.textContent = `Form submitted successfully! ${first_name}. `;
-        back.textContent = 'Back';
-       
+  
+    firstNameError.textContent = '';
+    firstNameError.style.display = "none";
+
+    seconNameError.textContent = '';
+    seconNameError.style.display = "none";
+
+    emailError.textContent = '';
+    emailError.style.display = "none";
+   
+}
+
+ 
+
+
+ document.getElementById("firstName").addEventListener("mouseover", function(){
+    firstHover.style.display = 'block';
+    firstHover.innerHTML = `Enter Your First Name.`;
+ });
+
+
+ document.getElementById("secondName").addEventListener("mouseover", function(){
+    secondHover.style.display = "block";
+    secondHover.innerHTML = `Enter you second Name.`;
+ });
+ document.getElementById("email").addEventListener("mouseover", function(){
+    emailHover.style.display = "block";
+    emailHover.innerHTML = `Enter Your Email.`;
+ });
+
+
+ document.getElementById("firstName").addEventListener("mouseout", function(){
+    firstHover.style.display = 'none';
+   
+ });
+
+
+ document.getElementById("secondName").addEventListener("mouseout", function(){
+    secondHover.style.display = "none";
+   
+ });
+ document.getElementById("email").addEventListener("mouseout", function(){
+    emailHover.style.display = "none";
+   
+ });
+
+ 
+
+
+ document.getElementById("firstName").addEventListener("input", clearError);
+ document.getElementById("secondName").addEventListener("input", clearError);
+ document.getElementById("email").addEventListener("input", clearError);
+
+
+
+document.getElementById("submit").onclick = function(e){
+    e.preventDefault();
+    const firstName = document.getElementById("firstName").value.trim();
+    const secondName = document.getElementById("secondName").value.trim();
+    const email = document.getElementById("email").value;
+
+    if(email.match(/^[^@]+@[^@]+\.[^@]+$/) && firstName && secondName){
+        my_field.innerHTML = `Hello ${firstName}, Your form Submitted succefully! <a href="formSubmit.html">Back</a>`;
     }
 
-    else if (!emailRegex.test(email.value)) {
-        errorDiv.textContent = "Please enter a valid email address.";
+    if(!email.match(/^[^@]+@[^@]+\.[^@]+$/))
+    {   emailError.style.display = "block";
+        emailError.innerHTML = `Double check your email!`;
     }
-    
-    
 
-    else{
+     if (!firstName){
+        firstNameError.style.display = "block";
+        firstNameError.innerHTML =`Fill out First Name field!`;
         
-
-        errorDiv.textContent = 'Please fill the form!';
-
-        
     }
+
+    if(!secondName){
+        seconNameError.style.display = "block";
+        seconNameError.innerHTML = `Fill out second Name field!`;
+
+    }
+
+
+   
+
 };
